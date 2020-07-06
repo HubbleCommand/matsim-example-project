7cef277e1fcb0a432d53e999ef78ab7fa10d3284
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-07-06 20:18:17 +0200

    Reservation start

--- src/main/java/org/sasha/reserver/ReservationManager.java
+++ src/main/java/org/sasha/reserver/ReservationManager.java
@@ -3,15 +3,16 @@ package org.sasha.reserver;
 import org.matsim.api.core.v01.network.Link;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public final class ReservationManager {
     private static ReservationManager INSTANCE;
     private String info = "Initial info class";
     private int timeInterval = 300; //Default 5-minute time interval, stored as seconds
-    private ArrayList<ReservationSlot> reservations;
 
     //ArrayList cannot have stuff added to an index bigger than it's current size
     //Hashmap will be easier
+    private HashMap<Integer, ReservationSlot> reservations; //Integer here stores the slot, relative to the time
 
     private ReservationManager() {
     }
@@ -28,12 +29,12 @@ public final class ReservationManager {
 
     //Only need time and link where reservation is needed
     public void makeReservation(int timeStart, int timeEnd, Link link){
-        int startSlot = time % timeInterval;
+        int startSlot = timeStart % timeInterval;
         for(int i = startSlot; i < ((timeEnd % timeInterval) - (timeStart % timeInterval)); i++){
-            if() {      // If no slot for this time exists
-                reservations.add(new ReservationSlot());
-            } else {    // Else update Slot Reserved Number
-                reservations.set(i)
+            if(reservations.containsKey(i)) {      // If a slot for this time exists
+                reservations.put(i, new ReservationSlot());
+            } else {                                // Else add new slot
+                reservations.put(i, new ReservationSlot());
             }
         }
     }
--- src/main/java/org/sasha/reserver/ReservationSlot.java
+++ src/main/java/org/sasha/reserver/ReservationSlot.java
@@ -1,5 +1,6 @@
 package org.sasha.reserver;
 
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
 
 import java.util.ArrayList;
@@ -7,6 +8,25 @@ import java.util.HashMap;
 
 public class ReservationSlot {
     private int slot;
-    private HashMap<Link, Integer> reservations;
+    private HashMap<Link, Integer> reservations; //Maps the link to the number of reservations
+    //TODO: use Link or LinkImpl?
+    //Need getID()
 
+    private HashMap<Id<Link>, Integer> reservations2; //Maps the link to the number of reservations
+
+    public ReservationSlot(){
+
+    }
+
+    public int getReservations(Link link){
+        return reservations.get(link);
+    }
+
+    public void makeReservation(Link link){
+        if(reservations.containsKey(link)){
+            reservations.put(link, reservations.get(link) + 1);
+        } else {
+            reservations.put(link, 1);
+        }
+    }
 }
