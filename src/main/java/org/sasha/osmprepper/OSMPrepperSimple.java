e83f331576c01d9c4a325140dbb05253719f1b27
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-07-24 18:26:00 +0200

    Made it more of a runnable

--- src/main/java/org/sasha/osmprepper/OSMPrepperSimple.java
+++ src/main/java/org/sasha/osmprepper/OSMPrepperSimple.java
@@ -16,15 +16,63 @@ import java.util.HashSet;
 
 //Code taken from https://simunto.com/matsim/tutorials/eifer2019/slides_day1.pdf
 public class OSMPrepperSimple {
+    private final String inputMapFilename;
+    private final String outputMapFilename;
+    private final String sourceCoordTransform;
+    private final String targetCoordTransform;
+    private final String[] modes;
+
+    public OSMPrepperSimple(String inputMapFilename, String outputMapFilename, String sourceCoordTransform, String targetCoordTransform, String[] modes){
+        this.inputMapFilename = inputMapFilename;
+        this.outputMapFilename = outputMapFilename;
+        this.sourceCoordTransform = sourceCoordTransform;
+        this.targetCoordTransform = targetCoordTransform;
+        this.modes = modes;
+    }
+
     public static void main(String[] args) {
-        CoordinateTransformation wgs84_utm32N = TransformationFactory.getCoordinateTransformation("EPSG:4326", "EPSG:25832");
+        String inputMapFilename = "";
+        String outputMapFilename = "";
+        String sourceCoordTransform = "";
+        String targetCoordTransform = "";
+        String[] modes = new String[1];
+
+        if ( args!=null && false ) {    //FIXME remove false if want to be runnable
+            if (args.length != 5) {
+                System.err.println("Usage: cmd inputMap.xml.gz outputMap.xml.gz EPSG:4326 EPSG:2056 car:bike:pt");
+                System.exit(401);
+            } else {
+                inputMapFilename = args[0] ;
+                outputMapFilename = args[1] ;
+                sourceCoordTransform = args[2] ;
+                targetCoordTransform = args[3] ;
+                modes = args[4].split(":");
+            }
+        } else {            //FIXME remove this else statement want to be runnable
+            inputMapFilename = "D:\\Files\\Uni\\Projet Bachelor\\matsim-sim\\scenarios\\geneva\\mappbfs\\genf_area_small.osm" ;
+            outputMapFilename = "D:\\Files\\Uni\\Projet Bachelor\\matsim-sim\\scenarios\\geneva\\mappbfs\\tmp\\network_clean_coordtransf.xml.gz";
+            sourceCoordTransform = "EPSG:4326" ;
+            targetCoordTransform = "EPSG:4326" ;
+            String modesStr = "car";
+            modes = modesStr.split(":");
+        }
+
+        OSMPrepperSimple app = new OSMPrepperSimple(inputMapFilename, outputMapFilename, sourceCoordTransform, targetCoordTransform, modes);
+        app.run();
+    }
+
+    private void run(){
+        CoordinateTransformation coordinateTransformation = TransformationFactory.getCoordinateTransformation(sourceCoordTransform, targetCoordTransform);
+
         Network network = NetworkUtils.createNetwork();
 
-        OsmNetworkReader reader = new OsmNetworkReader(network, wgs84_utm32N);
+        //OsmNetworkReader reader = new OsmNetworkReader(network, coordinateTransformation);
+        //Another method, but files are much bigger...
+        OsmNetworkReader reader = new OsmNetworkReader(network, coordinateTransformation, true, true);
         reader.setKeepPaths(true);
         reader.setMemoryOptimization(true);
 
-        reader.parse("D:\\Files\\Uni\\Projet Bachelor\\matsim-sim\\scenarios\\geneva\\mappbfs\\genf_area_small.osm");
+        reader.parse(inputMapFilename);
 
         //These lines are incredible! Makes the network file about 1/6 of the size!
         new NetworkSimplifier().run(network);
@@ -34,9 +82,9 @@ public class OSMPrepperSimple {
         //Add additional data to links (here need to add reservation mode to links)
         int count=1;
         for(Link link : network.getLinks().values()) {
-            link.setAllowedModes(new HashSet<>(Arrays.asList("car","car-reserved")));
+            link.setAllowedModes(new HashSet<>(Arrays.asList(modes)));
         }
 
-        new NetworkWriter(network).write("D:\\Files\\Uni\\Projet Bachelor\\matsim-sim\\scenarios\\geneva\\mappbfs\\tmp\\network_detailed_cleaned_wmodes.xml.gz");
+        new NetworkWriter(network).write(outputMapFilename);
     }
 }
