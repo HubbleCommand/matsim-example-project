7f23e1ba04cf9393e8d3b39ff5f552550ed27135
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-07-31 21:01:37 +0200

    Forgot to commit how to run it! That would've been a shame to forget.

--- src/main/java/org/matsim/project/RunMatsim.java
+++ src/main/java/org/matsim/project/RunMatsim.java
@@ -19,52 +19,68 @@
 package org.matsim.project;
 
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.contrib.otfvis.OTFVisLiveModule;
+import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.events.MatsimEventsReader;
+import org.matsim.core.events.handler.EventHandler;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.trafficmonitoring.FreeSpeedTravelTime;
 import org.matsim.run.RunBerlinScenario;
+import org.sasha.events.handlers.CongestionDetectionEventHandler;
+import org.sasha.reserver.ResetReservationsIterationEndsEventHandler;
 import org.sasha.routers.reservation.*;
 
 
-import java.net.URL;
-import java.util.Map;
-
 import com.google.inject.Key;
 import com.google.inject.name.Names;
-import org.matsim.api.core.v01.Coord;
-import org.matsim.api.core.v01.Id;
-import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
-import org.matsim.api.core.v01.network.Link;
-import org.matsim.core.config.Config;
-import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
-import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
-import org.matsim.core.controler.AbstractModule;
-import org.matsim.core.controler.Controler;
-import org.matsim.core.controler.OutputDirectoryHierarchy;
-import org.matsim.core.controler.OutputDirectoryLogging;
 import org.matsim.core.router.MainModeIdentifier;
 import org.matsim.core.router.MainModeIdentifierImpl;
 import org.matsim.core.router.RoutingModule;
-import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.core.utils.io.IOUtils;
-import org.matsim.examples.ExamplesUtils;
-import org.matsim.facilities.ActivityFacility;
-import org.matsim.facilities.ActivityOption;
-import org.matsim.utils.objectattributes.attributable.Attributes;
+
+import java.util.Map;
+//If need ExamplesUtils, just hover over it so that IntelliJ can automatically import it through Maven
+//import org.matsim.examples.ExamplesUtils;
+
 /**
  * @author sasha, based off of nagel's examples
- *
- *
- * Look here for doc on making custom scoring function:
- *  - https://github.com/matsim-org/matsim-code-examples/tree/ad3b07980f33ebf063aa19dc54b61a278d249f08/src/main/java/org/matsim/codeexamples/scoring/example16customscoring
- *
  * */
+
+/**
+ * (Doesn't seem to be useful) ride mode using teleportation
+ * 		https://github.com/matsim-org/matsim-code-examples/blob/12.x/src/main/java/org/matsim/codeexamples/programming/rideAsTeleportedCongestedMode/RunRideAsTeleportedCongestedModeExample.java
+ *
+ * (Might be useful?)
+ * 		MobSim pluggable trip router	:	https://github.com/matsim-org/matsim-code-examples/blob/11.x/src/main/java/org/matsim/codeexamples/mobsim/pluggableTripRouter/RunPluggableTripRouterExample.java
+ *		WithinDay replanning agents		:	https://github.com/matsim-org/matsim-code-examples/blob/12.x/src/main/java/org/matsim/codeexamples/withinday/withinDayReplanningAgents/RunWithinDayReplanningAgentExample.java
+ *		Custom MobSim Agents			:	https://github.com/matsim-org/matsim-code-examples/blob/11.x/src/main/java/org/matsim/codeexamples/mobsim/ownMobsimAgentUsingRouter/RunOwnMobsimAgentUsingRouterExample.java
+ *
+ * (Not needed anymore) Look here for doc on making custom scoring function:
+ * 		https://github.com/matsim-org/matsim-code-examples/tree/12.x/src/main/java/org/matsim/codeexamples/scoring/example16customscoring
+ *
+ * For router examples :
+ * 		https://github.com/matsim-org/matsim-code-examples/tree/12.x/src/main/java/org/matsim/codeexamples/router
+ *
+ * 	Documentation on doing stuff before sim (PrepareForSim / PrepareForMobSim)
+ * 		https://github.com/matsim-org/matsim-code-examples/tree/12.x/src/main/java/org/matsim/codeexamples/programming/ownPrepareForSimExample
+ * 		https://github.com/matsim-org/matsim-code-examples/tree/12.x/src/main/java/org/matsim/codeexamples/mobsim/ownMobsimAgentWithPerception
+ * 		https://github.com/matsim-eth/avtestbed-wip/blob/master/matsim/src/main/java/org/matsim/core/controler/PrepareForSimMultimodalImpl.java
+ *
+ * 	Examples on working with multiple modes:
+ * 		https://github.com/matsim-org/matsim-code-examples/blob/12.x/src/main/java/org/matsim/codeexamples/integration/RunMultipleModesExample.java
+ *
+ * 	Analyse plans:
+ * 		https://github.com/matsim-org/matsim-code-examples/blob/12.x/src/main/java/org/matsim/codeexamples/population/analyzePlans/AnalyzePlans.java
+ *
+ *
+ */
 public class RunMatsim{
 	public void runBerlinScenario(String[] args){
 		Config config = RunBerlinScenario.prepareConfig( args ) ;
@@ -84,97 +100,62 @@ public class RunMatsim{
 			config = ConfigUtils.loadConfig( args );
 		}
 		config.controler().setOverwriteFileSetting( OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists );
-
 		// possibly modify config here
+		config.qsim().getFlowCapFactor();	//TODO use for Disutility Function!!!
 
-		// ---
+		config.getModules();
 
 		Scenario scenario = ScenarioUtils.loadScenario(config) ;
-
 		// possibly modify scenario here
 
-		// ---
+		//Try to get the # of plans, but this has to be done with events, as plans may change per iteration with replanning!
+		/*for(Map.Entry p: scenario.getPopulation().getPersons().entrySet()) {		p.getValue();	}*/
 
 		Controler controler = new Controler( scenario ) ;
 
-		// possibly modify controler here
-
-
-		setupControllerMe2(controler, scenario);
-		//addCustomCostFactory(controler);
-		//addSimpleDoohiky(controler);
+		//Install my own module
+		System.out.println("Installing my module ...");
+		controler.addOverridingModule(new SimpleReservationModule(config, controler, scenario, "car"));
+		System.out.println("Installed my module !");
 
 		//Add this if want OTFVis live view thingy while simulation runs
 		//Can also just ask OTFVis to not sync in the interface
 		//controler.addOverridingModule( new OTFVisLiveModule() ) ;
 
+		setupCongestionDetector(controler, scenario);
+
 		controler.run();
 	}
 
-	public void addSimpleDoohiky(Controler controler){
-		controler.addOverridingModule(new SimpleReservationModule());
-	}
+	private void setupCongestionDetector(Controler controler, Scenario scenario){
+		//So I guess this version just allows to read event results? I honestly have no fucking idea
+		//why this was in the examples, because it doesn't seem to actually register the event handlers
+		/*EventsManager events = EventsUtils.createEventsManager();
 
-	public void addCustomCostFactory(Controler controler){
-		controler.addOverridingModule(new AbstractModule() {
-			@Override
-			public void install() {
-				bindLeastCostPathCalculatorFactory().to(SimpleReservationLeastCostPathCalculatorFactory.class);
+		CongestionDetectionEventHandler congestionDetectionEventHandler = new CongestionDetectionEventHandler(scenario.getNetwork());
+		events.addHandler(congestionDetectionEventHandler);*/
+
+		//Is this needed?
+		/*MatsimEventsReader reader = new MatsimEventsReader(events);
+		String inputFile = "output/example/output_events.xml.gz";
+		reader.readFile(inputFile);*/
+
+		//congestionDetectionEventHandler.writeCharts("output/totalCongestion.png", "output/averageCongestion.png");
+
+		//System.out.println("Events file read!");
+
+		//V2 actually works! Somewhat! Need to refer to logfile for results cause (read TODO in CongestionDetectionEventHandler)
+		controler.addOverridingModule(new AbstractModule(){
+			@Override public void install() {
+				/*this.addEventHandlerBinding().toInstance( new MyEventHandler1() );
+				this.addEventHandlerBinding().toInstance( new MyEventHandler2( ) );
+				this.addEventHandlerBinding().toInstance( new MyEventHandler3() );*/
+				this.addEventHandlerBinding().toInstance( new CongestionDetectionEventHandler( scenario.getNetwork() )  );
+
+				//This is needed for Startup and Shutdown listeners
+				//addControlerListenerBinding().toInstance(new CongestionDetectionEventHandler(scenario.getNetwork()));
 			}
 		});
-	}
-
-	public void setupControllerMe2(Controler controler, Scenario scenario){
-		controler.addOverridingModule(new AbstractModule() {
-			@Override
-			public void install() {
-				addRoutingModuleBinding("car").toProvider(
-						new SimpleReservationRoutingModuleProvider(
-								// the module uses the trip router for the PT part.
-								// This allows to automatically adapt to user settings,
-								// including if they are specified at a later stage
-								// in the initialisation process.
-								binder().getProvider(Key.get(RoutingModule.class, Names.named(TransportMode.pt))),
-								scenario.getPopulation().getFactory(),
-								//teleport,
-								controler.getScenario().getNetwork()
-						)
-				);
-				// we still need to provide a way to identify our trips
-				// as being teleportation trips.
-				// This is for instance used at re-routing.
-				bind(MainModeIdentifier.class).toInstance(new SimpleMainModeIdentifier(new MainModeIdentifierImpl()));
-			}
-		});
-	}
-
-	//installation of custom things done here to avoid duplicate code in runners
-	public void setupControllerMe(Controler controller){
-		//Add custom router
-		controller.addOverridingModule(new AbstractModule() {
-			@Override
-			public void install() {
-				//Example originally followed was
-				// https://github.com/matsim-org/matsim-code-examples/tree/ad3b07980f33ebf063aa19dc54b61a278d249f08/src/main/java/org/matsim/codeexamples/programming/leastCostPath
-				// https://github.com/matsim-org/matsim-code-examples/tree/11.x/src/main/java/org/matsim/codeexamples/programming/leastCostPath
-				//However this method is too complicated:
-				//Requires a complete re-implementation of a routing function
-				// which is not what is wanted, we just want to be able to use
-				// our own cost function and do the reservation once the route
-				// is calculated
-				//bindLeastCostPathCalculatorFactory().to(SimpleReservationRouterFactory.class);
-
-				//Use RoutingModule
-				addRoutingModuleBinding("car").to(SimpleReservationRoutingModule.class);
-				//addRoutingModuleBinding("car-reserve").to(SimpleReservationRoutingModule.class);
-
-			}
-		});
-		//Add const function that takes Reservation into account
-		//Look @ parking contrib
-
-		//Add event listeners for stuffs / scoring functions
-		//
 	}
 
 	public static void main(String[] args) {
