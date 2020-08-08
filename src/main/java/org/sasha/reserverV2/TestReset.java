f9b558635b431f42b958310e1e4c7ccca3b28564
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-08-08 23:37:51 +0200

    Additional failed attempts to easily reset...

--- src/main/java/org/sasha/reserverV2/TestReset.java
+++ src/main/java/org/sasha/reserverV2/TestReset.java
@@ -1,25 +1,30 @@
 package org.sasha.reserverV2;
 
 import org.apache.log4j.Logger;
+import org.matsim.core.controler.corelisteners.PlansReplanning;
 import org.matsim.core.controler.listener.ReplanningListener;
+import org.matsim.withinday.events.ReplanningEvent;
+import org.matsim.withinday.events.handler.ReplanningEventHandler;
 
 @Deprecated //This cannot do what I want it to do.... :(
 public class TestReset implements
-        //ReplanningEventHandler,   //Only throws when withinday replanning
-        ReplanningListener        //Doesn't give necessary information
-        //PlansReplanning           //Why does this one even exist?
+        ReplanningEventHandler,     //Only throws when withinday replanning
+        ReplanningListener,         //Doesn't give necessary information
+        PlansReplanning             //Why does this one even exist?
+
 {
     private static final Logger logger = Logger.getLogger(TestReset.class);
 
-    /*@Override
+    @Override
+    public void notifyReplanning(org.matsim.core.controler.events.ReplanningEvent event) {
+        System.out.println("Replanning here for some reason...");
+        //event.getReplanningContext().
+    }
+
+    @Override
     public void handleEvent(ReplanningEvent event) {
         System.out.println("Replanning for person: " + event.getPersonId());
         logger.warn("Replanning for person: " + event.getPersonId());
         //ReservationManager.getInstance().removeReservationsForPerson(event.getPersonId());
-    }*/
-
-    @Override
-    public void notifyReplanning(org.matsim.core.controler.events.ReplanningEvent event) {
-        //event.getReplanningContext().
     }
 }
